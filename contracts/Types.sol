fa1cd89e062e37c218b027ad91e43712cb3d9244
pythonpete32 <aaron@blackscale.io>
2023-04-14 23:24:03 +0400

    update Types

--- contracts/Types.sol
+++ contracts/Types.sol
@@ -28,6 +28,8 @@ enum OptimisticProposalStatus {
 /// @title OptimisticProposal
 /// @dev Represents a proposal in an optimistic governance system.
 struct OptimisticProposal {
+  DisputeStatus disputeStatus; // The status of the dispute related to the proposal.
+  OptimisticProposalStatus status; // The current status of the proposal.
   uint256 executionFromTime; // The earliest timestamp at which the proposal can be executed.
   uint256 pausedAtTime; // The timestamp at which the proposal was paused due to a challenge.
   uint256 disputeId; // The ID of the dispute related to the proposal.
@@ -36,8 +38,7 @@ struct OptimisticProposal {
   uint256 proposerPaidFees; // The amount of fees paid by the proposer.
   address challenger; // The address of the proposal's challenger.
   uint256 challengerPaidFees; // The amount of fees paid by the challenger.
-  IDAO.Action[] actions; // The actions associated with the proposal.
+  uint256 proposerCollateral; // The proposers collateral at stake above the fees for a rejected proposal
   uint256 allowFailureMap; // A map indicating which actions are allowed to fail during execution.
   bytes metadata; // Additional metadata related to the proposal.
-  OptimisticProposalStatus status; // The current status of the proposal.
 }
