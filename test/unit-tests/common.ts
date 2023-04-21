ceeea2bac4e568df466a79f4e6c15f8bbdc1a3de
pythonpete32 <aaron@blackscale.io>
2023-04-22 01:36:10 +0400

    add setup unit test

---
+++ test/unit-tests/common.ts
@@ -0,0 +1,33 @@
+import {ethers} from 'hardhat';
+
+export const abiCoder = ethers.utils.defaultAbiCoder;
+export const EMPTY_DATA = '0x';
+
+export const RULE_PERMISSION_ID = ethers.utils.id('RULE_PERMISSION');
+export const CREATE_PROPOSAL_PERMISSION_ID = ethers.utils.id(
+  'CREATE_PROPOSAL_PERMISSION'
+);
+export const CONFIGURE_PARAMETERS_PERMISSION_ID = ethers.utils.id(
+  'CONFIGURE_PARAMETERS_PERMISSION'
+);
+export const EXECUTE_PERMISSION_ID = ethers.utils.id('EXECUTE_PERMISSION');
+
+export const ADDRESS_ZERO = ethers.constants.AddressZero;
+export const ADDRESS_ONE = `0x${'0'.repeat(39)}1`;
+export const ADDRESS_TWO = `0x${'0'.repeat(39)}2`;
+export const NO_CONDITION = ADDRESS_ZERO;
+
+export const BN = ethers.BigNumber.from;
+
+export const DAYS_3 = BN(60 * 60 * 24 * 3);
+export const ARB_FEE = BN(ethers.utils.parseEther('0.069'));
+export const COLLATERAL = BN(ethers.utils.parseEther('1'));
+export const META_EVIDENCE = 'https://testing.com';
+export const EXTRA_DATA = '0x08575';
+export const INIT_ABI = ['address', 'uint256', 'uint256', 'string', 'bytes'];
+export const INIT_PARAMS = [
+  DAYS_3.toString(),
+  COLLATERAL.toString(),
+  META_EVIDENCE,
+  EXTRA_DATA,
+];
---
+++ test/unit-tests/optimistic-proposal-setup.test.ts
@@ -0,0 +1,144 @@
+import {
+  DAO,
+  OptimisticProposalSetup,
+  OptimisticProposalSetup__factory,
+  OptimisticProposalPlugin__factory,
+  CentralizedArbitrator__factory,
+  CentralizedArbitrator,
+} from '../../typechain';
+import {deployTestDao} from '../helpers/test-dao';
+import {Operation} from '../helpers/types';
+import {
+  ARB_FEE,
+  COLLATERAL,
+  CONFIGURE_PARAMETERS_PERMISSION_ID,
+  CREATE_PROPOSAL_PERMISSION_ID,
+  DAYS_3,
+  EXECUTE_PERMISSION_ID,
+  EXTRA_DATA,
+  INIT_ABI,
+  INIT_PARAMS,
+  META_EVIDENCE,
+  NO_CONDITION,
+  RULE_PERMISSION_ID,
+  abiCoder,
+} from './common';
+
+import {SignerWithAddress} from '@nomiclabs/hardhat-ethers/signers';
+import {expect} from 'chai';
+import {ethers} from 'hardhat';
+
+describe('OptimisticProposalSetup', function () {
+  let signers: SignerWithAddress[];
+  let OPSetup: OptimisticProposalSetup__factory;
+  let opSetup: OptimisticProposalSetup;
+  let Arbitraror: CentralizedArbitrator__factory;
+  let arbitrator: CentralizedArbitrator;
+  let dao: DAO;
+
+  before(async () => {
+    signers = await ethers.getSigners();
+    dao = await deployTestDao(signers[0]);
+
+    OPSetup = new OptimisticProposalSetup__factory(signers[0]);
+    opSetup = await OPSetup.deploy();
+
+    Arbitraror = new CentralizedArbitrator__factory(signers[0]);
+    arbitrator = await Arbitraror.deploy(ARB_FEE);
+  });
+
+  describe('prepareInstallation', async () => {
+    let initData: string;
+    const initParams = [arbitrator.address, ...INIT_PARAMS];
+
+    before(async () => {
+      initData = abiCoder.encode(initParams, INIT_ABI);
+    });
+
+    it('returns the plugin, helpers, and permissions', async () => {
+      const nonce = await ethers.provider.getTransactionCount(opSetup.address);
+      const anticipatedPluginAddress = ethers.utils.getContractAddress({
+        from: opSetup.address,
+        nonce,
+      });
+
+      const {
+        plugin,
+        preparedSetupData: {helpers, permissions},
+      } = await opSetup.callStatic.prepareInstallation(dao.address, initData);
+
+      expect(plugin).to.be.equal(anticipatedPluginAddress);
+      expect(helpers.length).to.be.equal(0);
+      expect(permissions.length).to.be.equal(4);
+      expect(permissions).to.deep.equal([
+        [
+          Operation.Grant,
+          plugin,
+          dao.address,
+          NO_CONDITION,
+          RULE_PERMISSION_ID,
+        ],
+        [
+          Operation.Grant,
+          plugin,
+          dao.address,
+          NO_CONDITION,
+          CREATE_PROPOSAL_PERMISSION_ID,
+        ],
+        [
+          Operation.Grant,
+          plugin,
+          dao.address,
+          NO_CONDITION,
+          CONFIGURE_PARAMETERS_PERMISSION_ID,
+        ],
+        [
+          Operation.Grant,
+          dao.address,
+          plugin,
+          NO_CONDITION,
+          EXECUTE_PERMISSION_ID,
+        ],
+      ]);
+
+      await opSetup.prepareInstallation(dao.address, initData);
+      const opPlugin = new OptimisticProposalPlugin__factory(signers[0]).attach(
+        plugin
+      );
+
+      // initialization is correct
+      expect(await opPlugin.dao()).to.eq(dao.address);
+      expect(await opPlugin.proposalCollateral()).to.be.eq(COLLATERAL);
+      expect(await opPlugin.executionDelay()).to.be.eq(DAYS_3);
+      expect(await opPlugin.arbitrator()).to.be.eq(arbitrator.address);
+      expect(await opPlugin.arbitratorExtraData()).to.be.eq(EXTRA_DATA);
+      expect(await opPlugin.metaEvidence()).to.be.eq(META_EVIDENCE);
+    });
+  });
+
+  // describe('prepareUninstallation', async () => {
+  //   it('returns the permissions', async () => {
+  //     const dummyAddr = ADDRESS_ZERO;
+
+  //     const permissions = await opSetup.callStatic.prepareUninstallation(
+  //       dao.address,
+  //       {
+  //         plugin: dummyAddr,
+  //         currentHelpers: [],
+  //         data: EMPTY_DATA,
+  //       }
+  //     );
+
+  //     expect(permissions.length).to.be.equal(1);
+  //     expect(permissions).to.deep.equal([
+  //       [
+  //         Operation.Revoke,
+  //         dummyAddr,
+  //         dao.address,
+  //         NO_CONDITION,
+  //         STORE_PERMISSION_ID,
+  //       ],
+  //     ]);
+  //   });
+  // });
+});
